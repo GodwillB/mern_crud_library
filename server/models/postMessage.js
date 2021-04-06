747b3657bcca8dc4e7ff956b88d57ee85b6036bd
Godwill Barasa <33608094+GodwillB@users.noreply.github.com>
2021-04-06 10:28:06 +0300

    Update postMessage.js

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -2,14 +2,10 @@ import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
     title: String,
-    message: String,
-    creator: String,
-    tags: [String],
+    description: String,
+    author: String,
+    isbn: [String],
     selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
     createdAt: {
         type: Date,
         default: new Date(),
